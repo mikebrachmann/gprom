82fb61275787fdf511367f16e0a511b619c28a72
Michael Brachmann <mrb24@buffalo.edu>
2017-02-14 11:19:04 -0500

    fixed memtest

--- travis/memtest.sh
+++ travis/memtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-/usr/bin/time -v "SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
+"SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
 SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
 SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
 SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
@@ -2201,7 +2201,7 @@ SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
 SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
 SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
 SELECT S.A AS P, U.C AS Q FROM R AS S JOIN T AS U ON S.A = U.C;
-" | gprom -log -loglevel 4 -backend sqlite -db ./debug.db -treeify-algebra-graphs
+" | /usr/bin/time -v gprom -log -loglevel 4 -backend sqlite -db ./debug.db -treeify-algebra-graphs
 
 
 
